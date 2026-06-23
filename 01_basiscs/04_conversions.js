419b2c4c1d66964f7a3fc2088dff81a418b44c15
uday-nagar <udaynagar07@gmail.com>
2026-06-23 20:07:07 +0530

    more conversions

---
+++ 01_basiscs/04_conversions.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);//for some resaon null doesnt get converted to 0 here 
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
