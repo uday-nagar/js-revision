670fd16d3dc4f54551bed6dabf1319ed837b4b90
uday-nagar <udaynagar07@gmail.com>
2026-06-23 19:05:44 +0530

    02

---
+++ 01_basiscs/02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "uday";
+let age = 18;
+let isLoggedIn = false;
+
+
+//number => 3 33 32 432424 32 4 
+//bigint 
+//string => "" or ''
+//boolean => true or false
+//null => representation of nothing
+//undefined 
+// symbol=> unique
+
+console.log(typeof name,typeof age,typeof isLoggedIn, typeof null, typeof undefined);
+
+// output           string    number      boolean            object        undefined 
