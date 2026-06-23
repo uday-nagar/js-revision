5378b4a690b72cff3de0cc9500196520e8a57f44
uday-nagar <udaynagar07@gmail.com>
2026-06-23 18:39:49 +0530

    Moved Git to the root js folder

---
+++ basics_01/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = "144144";
+let accountEmail = "uday@google.com";
+var accountPassword = "uday@123";
+accountCity = "Indore";
+
+
+console.log(accountId,accountEmail,accountPassword,accountCity);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+// avoid using var cuz it has global schope unlike let const that have block scope
