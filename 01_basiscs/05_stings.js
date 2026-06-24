e3dfbfb626e78083a4a289ce28d0e1bcc3fa25b5
uday-nagar <udaynagar07@gmail.com>
2026-06-24 18:02:33 +0530

    strings

---
+++ 01_basiscs/05_stings.js
@@ -0,0 +1,19 @@
+const name = "udayynaagar"
+const repoCount = 5
+
+console.log(`hello my name is ${name} and my github repo count is ${repoCount} `)
+console.log("udayyyy");
+
+devName = new String("udayy thedeveloper");//gives fucntions when we use strign object
+
+console.log(devName);
+console.log(devName.toUpperCase());
+console.log(devName.charAt(0));
+console.log(devName.indexOf('u'));
+console.log(devName.indexOf(' '));
+console.log(devName.substring(0,5));
+console.log(devName.trim());
+console.log("the naem is "+ devName.replace('u', 'n'));
+console.log(devName.split());
+
+//there are many more function we can always read mdn documentation according to reqirement 
