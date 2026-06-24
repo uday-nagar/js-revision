559f97400d538cb89b8127e8f4384db544e1537d
uday-nagar <udaynagar07@gmail.com>
2026-06-24 19:23:29 +0530

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+//arrays
+
+const myArr = [1,0,6,3,5,2,1,8];
+const myHeroes = ['spiderman', 'batman', 'loki','thor','stark'];
+const myArr2 = new Array(1,3,6,7);
+
+// Array methods
+
+myArr.push(6);
+myArr.push(7);
+myArr.pop();
+
+console.log(myArr);
