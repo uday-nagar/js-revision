a73446653daa6c22bd5b1de150d0ac0e5918c0a2
uday-nagar <udaynagar07@gmail.com>
2026-06-27 18:21:32 +0530

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,55 @@
+
+if(true)
+    {
+        let a = 10; 
+        const b = 20;
+        var c = 30;
+        
+}
+
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+
+
+function one(){
+    const username = "uday"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    }
+    // console.log(website) 
+       
+}
+if (true) {
+    const username = 'udayyy'
+    if (username == 'udayyy') {
+        const website = "youtube"
+        console.log(username+website)
+    }
+
+    // console.log(website);
+
+
+
+}
+
+// ++++++++++++++++INTRESTING++++++++++++++++++++++++++
+
+
+
+const addone = function (num){
+    if(typeof num == "number")
+    return num+1;
+    else
+        console.log('nahiiii');
+
+
+}
+
+console.log(addone('500'));
+
